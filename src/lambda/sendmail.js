94d67c1ba27030871b0e2bd96ee7fde560144cca
belke05 <you@example.com>
2019-11-22 14:58:15 +0100

    nodemail

--- src/lambda/sendmail.js
+++ src/lambda/sendmail.js
@@ -1,36 +1,43 @@
 const nodemailer = require("nodemailer");
+const { GPAS } = process.env;
 // const path = require("path");
 // require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 exports.handler = function(event, context, callback) {
   const { sender_mail, name, content, subject } = JSON.parse(event.body);
-  const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-      user: "henri160797@gmail.com",
-      pass: "77N47123"
-    }
-  });
-  const mailOptions = {
-    from: sender_mail,
-    to: "henri.dbel@gmail.com",
-    subject: `personal site mail from ${name} about ${subject}.`,
-    text: content
-  };
-  console.log(event.body, "event.body no json parse");
-  console.log(JSON.parse(event.body), "event.body");
-  smtpTransport
-    .sendMail(mailOptions)
-    .then(res => {
-      callback(null, {
-        statusCode: 200,
-        body: "Ok"
-      });
-      console.log("message sent", res);
-    })
-    .catch(err => {
-      callback(err);
-      console.error("error while sending message", err);
+
+  (async () => {
+    let account = await nodemailer.createTestAccount();
+    return account;
+  })().then(account => {
+    console.log(account, "-------------------", GPAS, "--------");
+    const smtpTransport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "henri160797@gmail.com", // generated ethereal user
+        pass: "77N47123" // generated ethereal password
+      }
     });
+    const mailOptions = {
+      from: sender_mail,
+      to: "henri.dbel@gmail.com",
+      subject: `personal site mail from ${name} about ${subject}.`,
+      text: content
+    };
+    console.log(event.body, "event.body no json parse");
+    console.log(JSON.parse(event.body), "event.body");
+    smtpTransport
+      .sendMail(mailOptions)
+      .then(res => {
+        callback(null, {
+          statusCode: 200,
+          body: "Ok"
+        });
+        console.log("message sent", res);
+      })
+      .catch(err => {
+        callback(err);
+        console.error("error while sending message", err);
+      });
+  });
 };
